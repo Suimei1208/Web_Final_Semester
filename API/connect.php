b9223e6c8d040e01bcc8f42aac605caf91574a1e
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-05 20:17:30 +0700

    Update connect.php

--- API/connect.php
+++ API/connect.php
@@ -20,4 +20,18 @@
         }else return false;
         $conn->close();
     }
+    function getGenres(){
+        $conn = connect();
+        $sql = "SELECT * FROM genres";
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result)>0){
+            $items = [];
+            while($row = mysqli_fetch_assoc($result)){
+                $items[] = $row;
+            }
+            return $items;
+        }else return false;
+        $conn->close();
+    }
 ?>
