121015508718bcb4d9565f2415671be75ce3c6b1
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-07 17:08:45 +0700

    Update search.php

--- search.php
+++ search.php
@@ -32,7 +32,7 @@
 
     <main style="margin-top: 80px;">
     <div class="update content">
-        <strong class="up">Search for: NTR</strong>
+        <strong class="up">Search for:</strong>
     </div>
     <div class="update">
     <?php
