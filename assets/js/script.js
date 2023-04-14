f7fd81f6fde14f8e54043dd40301a7307003d6bc
Waankiu <129172377+Waankiu@users.noreply.github.com>
2023-04-14 23:25:16 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,48 @@
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
-let currentSlide = 1;
+var btns = document.getElementsByClassName('btn');
+var currentSlide = 1;
 
-// Javascript cho image slider
+// Function for manual navigation
 var manualNav = function(manual){
-    slides.forEach((slide) =>{
-        slide.classList.remove('active');
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].classList.toggle('active', i === manual);
+        btns[i].classList.toggle('active', i === manual);
+    }
+    currentSlide = manual;
+};
 
-        btns.forEach((btn) =>{
-            btn.classList.remove('active');
-        })
-    })
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
+// Add event listeners to the buttons
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var manual = parseInt(this.getAttribute('data-slide-to'));
+        manualNav(manual);
+    });
 }
-btns.forEach((btn,i) => {
-    btn.addEventListener("click",() =>{
-        manualNav(i);
-        currentSlide = i;
-    })
-})
-//autoplay
+
+// Function for autoplay
 var repeat = function(activeClass){
-    let active = document.getElementsByClassName('active');
-    let i = 1;
+    var active = document.getElementsByClassName('active');
+    var i = 1;
 
-    var repeater = () =>{
+    var repeater = function() {
         setTimeout(function(){
-
-            slides[i].classList.add('active');
-            btns[i].classList.add('active');
+            for (var j = 0; j < slides.length; j++) {
+                slides[j].classList.toggle('active', j === i);
+                btns[j].classList.toggle('active', j === i);
+            }
+            currentSlide = i;
             i++;
-            if(slides.length == i ){
+            if (i >= slides.length) {
                 i = 0;
             }
-            if(i >=slides.length){
-                return;
-            }
             repeater();
-        },5000);
-
-    }
+        }, 5000);
+    };
     repeater();
-}
+};
+
+// Start autoplay
 repeat();
+
 
 // var headerHeight = document.querySelector('header').offsetHeight;
 // var mainHeight = document.querySelector('main').offsetHeight;
@@ -138,7 +137,7 @@ function getPageList(totalPages, page, maxLength) {
       return showPage(currentPage - 1);
     });
   });
-  
+
 //tooltip
 var spanText = document.getElementsByClassName('tooltip');
 
