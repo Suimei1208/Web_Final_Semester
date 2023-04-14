c947d59d53c8204fbee8b660d64cad486daecf3e
Waankiu <129172377+Waankiu@users.noreply.github.com>
2023-04-14 23:53:59 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,49 @@
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
 
 // Javascript cho image slider
 var manualNav = function(manual){
+    slides.forEach((slide) =>{
+        slide.classList.remove('active');
 
         btns.forEach((btn) =>{
+            btn.classList.remove('active');
+        });
     });
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
 }
 btns.forEach((btn,i) => {
+    btn.addEventListener("click",() =>{
+        manualNav(i);
+        currentSlide = i;
+    })
+})
+//autoplay
 var repeat = function(activeClass){
     let active = document.getElementsByClassName('active');
+    let i = 1;
 
     var repeater = () =>{
         setTimeout(function(){
-                slides[j].classList.toggle('active', j === i);
-                btns[j].classList.toggle('active', j === i);
-            }
-            currentSlide = i;
+            [...active].forEach((activeSlide) =>{
+                activeSlide.classList.remove('active');
+            });
             slides[i].classList.add('active');
             btns[i].classList.add('active');
             i++;
-            if (i >= slides.length) {
             if(slides.length == i ){
                 i = 0;
             }
+            if(i >=slides.length){
+                return;
             }
             repeater();
-};
         },5000);
 
-// Start autoplay
+    }
+    repeater();
 }
 repeat();
 
