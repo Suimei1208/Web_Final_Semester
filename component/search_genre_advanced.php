9d9276d5fa7a5c4fff1dd1ead01c3da52b50f7e8
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-16 18:26:22 +0700

    fix code

--- component/search_genre_advanced.php
+++ component/search_genre_advanced.php
@@ -49,47 +49,49 @@
             echo "<h3 class='title'>Search for: " . implode(", ", $selected_genres) . "</h3>";
             $name_flim_search = getFlims_Genre_advan($selected_genres); 
             if($name_flim_search == false) require_once('component/404.php');
-            else{ ?>     
+            else{ ?>  
+            <div class="update">
+                <div class="card-content" style="display: none;">   
                <?php if($name_flim_search ) { ?>
-                    <div class="update">
-                    <div class="card-content" style="display: none;"> <?php
-                    foreach($name_flim_search as $p){
-                        if($p['poster_small'] == null) $p['poster_small'] = 'fake.png';
-                                if($p['name_flim'] == null) $p['name_flim'] = 'Updating...';   
-                                if($p['actor'] == null) $p['actor'] = 'Updating...';                
-                        ?>
-                        <a href="info.php?movie_name=<?=$p['name_flim']?>" class = "color_info">
-                            <div class="card">
-                                <div class="card-image">
-                                    <img src="assets/img/<?=$p['poster_small']?>" alt=""></div>
-                                <div class="tooltip">
-                                <div class="item">
-                                    <div class="NameFilm"><?=$p['name_flim']?></div>
-                                    <div class="Story"><?=preg_replace("/\r\n|\r|\n/","<br>", $p['content']);?> </div> 
-                                    <div class="STD" style="color: #fffb00">
-                                            <span style="margin-right: 10px;">
-                                                <i class="fa-solid fa-star"></i><span class="col-while" style="color: #000000"> <?=$p['rate']?>/5 </span> 
-                                            </span>
-                                            <span style="margin-right: 10px;">
-                                                <i class="fa-regular fa-clock" ></i> <span class="col-while" style="color: #000000"><?=$p['time']?> minutes</span> 
-                                            </span>                
-                                            <i class="fa-regular fa-calendar"></i> <span  class="col-while" style="color: #000000"><?=$p['year']?></span>
+                <?php 
+                            foreach($name_flim_search as $p){
+                                if($p['poster_small'] == null) $p['poster_small'] = 'fake.png';
+                                        if($p['name_flim'] == null) $p['name_flim'] = 'Updating...';   
+                                        if($p['actor'] == null) $p['actor'] = 'Updating...';                
+                                ?>
+                                <a href="info.php?movie_name=<?=$p['name_flim']?>" class = "color_info">
+                                    <div class="card">
+                                        <div class="card-image">
+                                            <img src="assets/img/<?=$p['poster_small']?>" alt=""></div>
+                                        <div class="tooltip">
+                                            <div class="item">
+                                                <div class="NameFilm"><?=$p['name_flim']?></div>
+                                                <div class="Story"><?=preg_replace("/\r\n|\r|\n/","<br>", $p['content']);?> </div> 
+                                                <div class="STD" style="color: #fffb00">
+                                                        <span style="margin-right: 10px;">
+                                                            <i class="fa-solid fa-star"></i><span class="col-while" style="color: #000000"> <?=$p['rate']?>/5 </span> 
+                                                        </span>
+                                                        <span style="margin-right: 10px;">
+                                                            <i class="fa-regular fa-clock" ></i> <span class="col-while" style="color: #000000"><?=$p['time']?> minutes</span> 
+                                                        </span>                
+                                                        <i class="fa-regular fa-calendar"></i> <span  class="col-while" style="color: #000000"><?=$p['year']?></span>
+                                                </div>
+                                                <div class="AG">
+                                                    <div class="AU"><span>Director:</span> <?=$p['director']?></div>
+                                                    <div class="AC"><span>Actor:</span> <?=$p['actor']?></div>
+                                                    <div class="GE"><span>Genres:</span> <?=$p['genre']?></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="card-info">
+                                            <h3><?=$p['name_flim']?></h3>
+                                            <p>View: <?=number_format($p['view'])?></p>
+                                        </div>
                                     </div>
-                                    <div class="AG">
-                                        <div class="AU"><span>Director:</span> <?=$p['director']?></div>
-                                        <div class="AC"><span>Actor:</span> <?=$p['actor']?></div>
-                                        <div class="GE"><span>Genres:</span> <?=$p['genre']?></div>
-                                    </div>
-                                </div>
-                            </div>
-                                <div class="card-info">
-                                    <h3><?=$p['name_flim']?></h3>
-                                    <p>View: <?=number_format($p['view'])?></p>
-                                </div>
-                            </div>
-                        </a>
+                                </a>
+
                 <?php }  
-                echo '<div class="pagination"></div>';                           
+                echo '</div> <div class="pagination"></div>';                           
             }}}}else{
                     echo "<style>
                     .advance{
