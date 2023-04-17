08dce49df0a8e1cb82ec34d29525930adb53357e
Waankiu <129172377+Waankiu@users.noreply.github.com>
2023-04-17 21:00:33 +0700

    fuk

--- assets/js/script.js
+++ assets/js/script.js
@@ -289,3 +289,21 @@ if (dislikeButton.classList.contains('active')) {
 }
 }
 });
+//Dropdown Login
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn-Log')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content-Log");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
