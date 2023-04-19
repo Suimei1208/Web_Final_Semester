001b5e512b0f8b3831c12e5a46f5c7d25dfed359
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-19 10:20:45 +0700

    up

--- text.php
+++ text.php
@@ -1,14 +1,10 @@
 <?php 
-    include("API/connect.php");
-    $name= get_film("Solo Leveling");
-    foreach ($name as $film) {
-        echo "Tên phim: " . $film["name_flim"] . "<br>";
-        echo "Thể loại: " . $film["genre"] . "<br>";
-        echo "Đạo diễn: " . $film["director"] . "<br>";
-        echo "Năm sản xuất: " . $film["year"] . "<br>";
-        echo "Thời lượng: " . $film["time"] . " phút" . "<br>";
-        echo "Nội dung: " . $film["content"] . "<br>";
-        echo "Diễn viên: " . $film["actor"] . "<br>";
-    }
+    $ten = "Dungeons & Dragons: Honor Among Thieves";
+    $kyTuDacBiet = array("&");
+    $kyTuThayThe = array("\\&");
+    $tenDaThemGachCheo = str_replace($kyTuDacBiet, $kyTuThayThe, $ten);
+    echo $tenDaThemGachCheo;
+    
+    
 
 ?>
