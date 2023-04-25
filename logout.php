15c4c6aa0106f7db730a0db76e36d939cc66a6ce
Suimei1208 <103234140+Suimei1208@users.noreply.github.com>
2023-04-25 17:45:01 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,18 @@
 <?php
-    session_start();
-    $current_url = $_SESSION['current_url']; 
-    session_unset();
-    session_destroy();
-    setcookie('username', '', time() - 3600, '/');
-    setcookie('password', '', time() - 3600, '/');
-    header('Location: '.$current_url); 
+session_start();
+$current_url = $_SESSION['current_url'];
+session_unset();
+session_destroy();
+setcookie('username', '', time() - 3600, '/');
+setcookie('password', '', time() - 3600, '/');
+
+$exclude_pages = array('library.php', 'your_info.php');
+
+if (strpos($current_url, 'username=') !== false) {
+    header('Location: index.php');
     exit();
+} else {
+    header('Location: '.$current_url);
+    exit();
+}
 ?>
